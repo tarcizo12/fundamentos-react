c820f5fdca3a6a620efe4c0de7a65f9e1b468ce8
Tarcizo Neto <tarcizon12@gmail.com>
2023-03-02 12:10:08 -0300

    COnhecendo JSX

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom'
+import React from 'react'
 
 const element = document.getElementById('root')
+const tag = <strong> Olá React! </strong> //tem que estar dentro de uma tag != de div
 
-ReactDOM.render('Ola mundo',element)
+ReactDOM.render(
+    <div>
+       { tag }
+    </div>,
+    element
+)
